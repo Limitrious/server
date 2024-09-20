8a3d283f9be83650f2171d3d8a59c47961e3bb2b
lamatus <85483417+Limitrious@users.noreply.github.com>
2024-09-20 18:53:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const statusElement = document.getElementById('demo');
 statusElement.innerText = 'Checking status...'; 
 
-const apiUrl = 'https://api.mcstatus.io/v2/status/java/98.70.88.239:25565'; 
+const apiUrl = 'https://api.mcstatus.io/v2/status/java/147.185.221.20:62649'; 
 
 const updateStatus = () => {
 
